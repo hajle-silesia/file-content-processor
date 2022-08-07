1da7feb7aea270890164518f3c4417ae4e7b3cc8
mtweeman <mtweeman@gmail.com>
2022-08-07 21:14:42 +0200

    Implemented strategy context

---
+++ src/strategy_context.py
@@ -0,0 +1,15 @@
+from abc import ABC
+
+
+class StrategyContext(ABC):
+    def __init__(self, *strategies):
+        self._strategies = strategies
+
+    def process(self, raw_content):
+        content = []
+        for entry in raw_content:
+            processed_entry = {}
+            for strategy in self._strategies:
+                processed_entry.update(strategy.process(entry))
+            content.append(processed_entry)
+        return content
