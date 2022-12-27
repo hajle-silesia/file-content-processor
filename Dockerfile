3c26e91518e069a59cc279bb15518eddfc2f22f0
mtweeman <mtweeman@gmail.com>
2022-12-27 21:59:47 +0100

    Added Kafka (#3)

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,8 @@ FROM python:3.10.5
 RUN apt update && apt install -y \
     curl
 WORKDIR project
+ENV PYTHONPATH "${PYTHONPATH}:/project"
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 COPY ./src ./src
-ENV PYTHONPATH "${PYTHONPATH}:/project"
 ENTRYPOINT ["python", "src/main.py"]
--- tests/test_file_content_processor.py
+++ tests/test_file_content_processor.py
@@ -2,10 +2,6 @@ import json
 import pathlib
 import unittest.mock
 
-import common.notifier
-import common.storage
-import common.utils
-
 from src.category_filter import *
 from src.file_content_processor import FileContentProcessor
 from src.processor import Processor
@@ -20,37 +16,20 @@ with open(pathlib.Path(__file__).parent / "./files/file_content_processor_return
           encoding="utf-8") as file_content_processor_return_value_json:
     expected_return_value = json.load(file_content_processor_return_value_json)
 
-url = {'observer': "http://observer/update"}
+
+class MockProducer:
+    def send(self, topic, value):
+        pass
 
 
-def mocked_requests_post(*args, **kwargs):
-    class MockResponse:
-        def __init__(self, json_data, status_code):
-            self.json_data = json_data
-            self.status_code = status_code
-
-        def json(self):
-            return self.json_data
-
-    if args[0] in url.values():
-        return MockResponse(None, 204)
-    else:
-        return MockResponse(None, 404)
-
-
-@unittest.mock.patch("common.notifier.requests.post", side_effect=mocked_requests_post)
 class TestFileContentProcessor(unittest.TestCase):
-    storage = None
-    storage_path = None
-    notifier = None
+    producer = None
 
     @classmethod
     def setUpClass(cls):
-        cls.storage = common.storage.Storage()
-        cls.storage_path = pathlib.Path(__file__).parent / "./data.json"
-        cls.storage.path = cls.storage_path
-        cls.notifier = common.notifier.Notifier(cls.storage)
-        cls.notifier.register_observer(url)
+        super().setUpClass()
+
+        cls.producer = MockProducer()
 
         hops_processor = Processor(recipe_filter=RecipesFilter(),
                                    category_filter=HopsFilter(),
@@ -86,73 +65,61 @@ class TestFileContentProcessor(unittest.TestCase):
                           'parameters': parameters_processor,
                           }
 
-    @classmethod
-    def tearDownClass(cls):
-        super().tearDownClass()
-
-        common.utils.remove_file(cls.storage_path)
-
     def setUp(self):
         super().setUp()
 
-        self.file_content_processor = FileContentProcessor(notifier=self.notifier, processors=self.processors)
+        self.file_content_processor = FileContentProcessor(producer=self.producer, processors=self.processors)
 
-    def test_Should_GetEmptyContent_When_GivenNoneRawContent(self, mock_get):
+    def test_Should_GetEmptyContent_When_GivenNoneRawContent(self):
         self.file_content_processor.update(None)
 
         self.assertEqual({}, self.file_content_processor.content)
 
-    def test_Should_GetEmptyContent_When_NoneRawContentBecomesEmptyRawContent(self, mock_get):
+    def test_Should_GetEmptyContent_When_NoneRawContentBecomesEmptyRawContent(self):
         self.file_content_processor.update(None)
         self.file_content_processor.update({})
 
         self.assertEqual({}, self.file_content_processor.content)
 
-    def test_Should_GetEmptyContent_When_GivenEmptyRawContent(self, mock_get):
+    def test_Should_GetEmptyContent_When_GivenEmptyRawContent(self):
         self.file_content_processor.update({})
 
         self.assertEqual({}, self.file_content_processor.content)
 
-    def test_Should_GetContent_When_EmptyRawContentBecomesNonemptyRawContent(self, mock_get):
+    def test_Should_GetContent_When_EmptyRawContentBecomesNonemptyRawContent(self):
         self.file_content_processor.update({})
         self.file_content_processor.update(recipe)
 
         self.assertEqual(expected_return_value,
                          json.loads(json.dumps(self.file_content_processor.content, default=str)))
-        self.assertEqual(204, self.notifier.responses[0].status_code)
-        self.assertEqual(None, self.notifier.responses[0].json())
 
-    def test_Should_GetContent_When_GivenNonemptyRawContent(self, mock_get):
+    def test_Should_GetContent_When_GivenNonemptyRawContent(self):
         self.file_content_processor.update(recipe)
 
         self.assertEqual(expected_return_value,
                          json.loads(json.dumps(self.file_content_processor.content, default=str)))
-        self.assertEqual(204, self.notifier.responses[0].status_code)
-        self.assertEqual(None, self.notifier.responses[0].json())
 
-    def test_Should_GetContent_When_NoneRawContentBecomesNonemptyRawContent(self, mock_get):
+    def test_Should_GetContent_When_NoneRawContentBecomesNonemptyRawContent(self):
         self.file_content_processor.update(None)
         self.file_content_processor.update(recipe)
 
         self.assertEqual(expected_return_value,
                          json.loads(json.dumps(self.file_content_processor.content, default=str)))
-        self.assertEqual(204, self.notifier.responses[0].status_code)
-        self.assertEqual(None, self.notifier.responses[0].json())
 
-    def test_Should_GetContent_When_NonemptyRawContentBecomesEmptyRawContent(self, mock_get):
+    def test_Should_GetContent_When_NonemptyRawContentBecomesEmptyRawContent(self):
         self.file_content_processor.update(recipe)
         self.file_content_processor.update({})
 
         self.assertEqual(expected_return_value,
                          json.loads(json.dumps(self.file_content_processor.content, default=str)))
 
-    def test_Should_GetEmptyContent_When_EmptyRawContentBecomesNoneRawContent(self, mock_get):
+    def test_Should_GetEmptyContent_When_EmptyRawContentBecomesNoneRawContent(self):
         self.file_content_processor.update({})
         self.file_content_processor.update(None)
 
         self.assertEqual({}, self.file_content_processor.content)
 
-    def test_Should_GetContent_When_NonemptyRawContentBecomesNoneRawContent(self, mock_get):
+    def test_Should_GetContent_When_NonemptyRawContentBecomesNoneRawContent(self):
         self.file_content_processor.update(recipe)
         self.file_content_processor.update(None)
 
